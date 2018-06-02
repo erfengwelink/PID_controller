f2dbb65cdba65f5f95d8dbd049d9e844b7487a22
erfengwelink <erfengwelink@welink.io>
2018-06-02 10:56:49 +0800

    rename tutorial/ 's all dirs' name

---
+++ tutorial/0-1/main.c
@@ -0,0 +1,17 @@
+
+
+#define a 2
+#define b 3
+#define c (float)(a*b)/5
+
+int main()
+{
+	if(a==4)
+		printf("c=%f\n",c);
+	else if(a==3)
+	{}	
+	else if(b==3)
+		printf("c=%f\n",c);
+	return 0;
+}
+
